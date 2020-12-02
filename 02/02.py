9a4182c602d85434cc2d001caa2a5f3cd993ab9d
Stefan Brandl <steve260897@gmail.com>
2020-12-02 12:10:57 +0100

    day 02

---
+++ 02/02.py
@@ -0,0 +1,30 @@
+import re
+from typing import List, NamedTuple
+
+
+class Password(NamedTuple):
+    i1: int
+    i2: int
+    needle: str
+    haystack: str
+
+    @classmethod
+    def from_string(cls, string: str) -> 'Password':
+        match = re.match(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', string)
+        return cls(int(match.group(1)), int(match.group(2)), match.group(3), match.group(4))
+
+
+def part1(data: List[Password]) -> int:
+    return sum(p.i1 <= p.haystack.count(p.needle) <= p.i2 for p in data)
+
+
+def part2(data: List[Password]) -> int:
+    return sum((p.haystack[p.i1 - 1] == p.needle) != (p.needle == p.haystack[p.i2 - 1]) for p in data)
+
+
+if __name__ == '__main__':
+    with open('02/input.txt', 'r') as in_file:
+        data = [Password.from_string(line) for line in in_file]
+
+    print(part1(data))
+    print(part2(data))

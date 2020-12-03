070764ec8a87c4b0c72261d7aab114ab8f33f28e
Stefan Brandl <steve260897@gmail.com>
2020-12-03 12:07:09 +0100

    day 03

---
+++ 03/03.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from math import prod
+from typing import Sequence
+
+
+@dataclass
+class Vector:
+    x: int
+    y: int
+
+    def __mod__(self, x: int) -> 'Vector':
+        return Vector(self.x % x, self.y)
+
+    def __mul__(self, mul: int) -> 'Vector':
+        return Vector(self.x * mul, self.y * mul)
+
+
+def part1(data: Sequence[Sequence[bool]], direction: Vector) -> int:
+    return sum(data[p.y][p.x] for p in [(direction * i) % len(data[0]) for i in range(len(data) // direction.y)])
+
+
+def part2(data: Sequence[Sequence[bool]], directions: Sequence[Vector]) -> int:
+    return prod(part1(data, di) for di in directions)
+
+
+if __name__ == '__main__':
+    with open('03/input.txt', 'r') as in_file:
+        data = [[ch == '#' for ch in line[:-1]] for line in in_file]
+
+    print(part1(data, Vector(3, 1)))
+    print(part2(data, [Vector(1, 1), Vector(3, 1), Vector(5, 1), Vector(7, 1), Vector(1, 2)]))

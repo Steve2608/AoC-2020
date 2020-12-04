41136789190f0e36db22d217b0c030746600ff11
Stefan Brandl <steve260897@gmail.com>
2020-12-04 13:19:53 +0100

    enforced fullmatch for regexp

--- 02/02.py
+++ 02/02.py
@@ -12,7 +12,7 @@ class Password:
 
     @classmethod
     def from_string(cls, string: str) -> 'Password':
-        match = re.match(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', string)
+        match = re.fullmatch(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', string)
         return cls(int(match.group(1)), int(match.group(2)), match.group(3), match.group(4))
 
 
@@ -26,7 +26,7 @@ def part2(data: Sequence[Password]) -> int:
 
 if __name__ == '__main__':
     with open('02/input.txt', 'r') as in_file:
-        data = [Password.from_string(line) for line in in_file]
+        data = [Password.from_string(line.replace('\n', '')) for line in in_file]
 
     print(part1(data))
     print(part2(data))

a343ebe718c10eaf99cbf42b0d1b71a01f35d504
Stefan Brandl <steve260897@gmail.com>
2020-12-16 20:03:18 +0100

    updated all to take whatever python interpreter

--- all.py
+++ all.py
@@ -1,12 +1,15 @@
+import sys
 from pathlib import Path
 from subprocess import call
 
 
 if __name__ == '__main__':
-    all_files = sorted(Path('.').rglob('./*.py'))
+    interpreter = sys.argv[1]
+
+    all_files = sorted(Path('.').rglob(r'./*.py'))
     all_files.remove(Path('all.py'))
     for i, path in enumerate(all_files, 1):
         print(f'day {i:02d}:')
         # call(f'python {path}'.split())
-        call(f'python3.8 {path}'.split())
+        call(f'{interpreter} {path}'.split())
         print()

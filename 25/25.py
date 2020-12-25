9e3694556f1a06d4fbe0ae76810ee686127e9bb8
Stefan Brandl <steve260897@gmail.com>
2020-12-25 11:03:49 +0100

    added typehints, hard-coded inputs

--- 25/25.py
+++ 25/25.py
@@ -1,7 +1,7 @@
 from functools import partial
 
 
-def transform(target: int, /, subect_number: int, mod: int = 20201227):
+def transform(target: int, /, subect_number: int, mod: int = 20201227) -> int:
     n, value = 0, 1
     while value != target:
         n += 1
@@ -10,7 +10,7 @@ def transform(target: int, /, subect_number: int, mod: int = 20201227):
     return n
 
 
-def transform_n(n: int, /, subect_number: int, mod: int = 20201227):
+def transform_n(n: int, /, subect_number: int, mod: int = 20201227) -> int:
     value = 1
     for _ in range(n):
         value *= subect_number
@@ -18,13 +18,11 @@ def transform_n(n: int, /, subect_number: int, mod: int = 20201227):
     return value
 
 
-def part1(card: int, door: int):
-    n_card = transform(card, subect_number=7)
+def part1(card: int, door: int) -> int:
     n_door = transform(door, subect_number=7)
-
     key = transform_n(n_door, subect_number=card)
-    assert transform_n(n_card, subect_number=door) == key
 
+    assert transform_n(transform(card, subect_number=7), subect_number=door) == key
     return key
 
 
@@ -34,7 +32,4 @@ example1 = partial(part1, card=5764801, door=17807724)
 if __name__ == "__main__":
     assert example1() == 14897079
 
-    with open('25/input.txt', 'r') as in_file:
-        card, door = map(int, in_file.read().strip().splitlines())
-
-    print(part1(card, door))
+    print(part1(card=6270530, door=14540258))
